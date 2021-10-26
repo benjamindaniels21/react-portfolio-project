a50a37ee58d231b3bd7739f1008b4536f4c4bd39
Ben Daniels <benjamindaniels21@gmail.com>
2021-10-25 20:26:28 -0400

    start nav add reactstrap

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+      {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links',
+    },
+      {
+        title: 'Staff',
+        url: '#',
+        cName: 'nav-links',
+    },
+      {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+      {
+        title: 'Mobile',
+        url: '#',
+        cName: 'nav-links-mobile',
+    }
+]
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="NavBarItem">
+                <h1 className="navbar-logo">NFM Indoor</h1>
+                <div className="menu-icon">
+                    
+                </div>
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
